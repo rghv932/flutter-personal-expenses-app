b4adf8906f2f5ab87307c6155232e6a49b631186
Raghav Maheshwari <raghav.maheshwari@nagarro.com>
2022-11-02 21:40:25 +0530

    Modal Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './widgets/user_transactions.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,20 +23,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // final List<Transaction> transactions = [];
-  // late String titleInput;
-  // late String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  // String titleInput;
+  // String amountInput;
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 100, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Books', amount: 200, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Books', amount: 200, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Books', amount: 200, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Books', amount: 200, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter - Personal Expenses App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: ()=>_startAddNewTransaction(context),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -46,12 +90,17 @@ class MyHomePage extends StatelessWidget {
               child: Card(
                 color: Colors.blue,
                 elevation: 5,
-                child: Text('Chart!'),
+                child: Text('CHART!'),
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: ()=>_startAddNewTransaction(context),
       ),
     );
   }
